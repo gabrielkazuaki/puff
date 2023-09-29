80fe96fb33639232e7115d0ddcdb8bccb808a07f
gabrielkazuaki <128142107+gabrielkazuaki@users.noreply.github.com>
2023-09-29 08:49:43 -0300

    Add files via upload

---
+++ aluramidi/main.js
@@ -0,0 +1,11 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+while () {
+    listaDeTeclas[0].onclick = tocaSomPom;
+}
